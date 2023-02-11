709e9d5eee4a6a35012a830210d008a34b9cbb12
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2023-02-11 15:21:47 +0900

    Fixed：rename folder
    フォルダの名前が使ってるライブラリの名前と被って使えないので修正．

--- flaskAPI/app.py
+++ flaskAPI/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_session import Session
-from flask.setAPI import *
+from flaskAPI.setAPI import *
 
 app = Flask(__name__)
 api = Api(app)
