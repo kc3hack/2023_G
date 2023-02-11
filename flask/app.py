b90cb4fa189d6eba8347bd217e2ad358132268ac
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2023-02-11 15:16:35 +0900

    Add: default api sources
    今までに使っていたAPIのflaskファイルを追加．これを元にAPI実装をしていく．

---
+++ flask/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_restful import Api
+from flask_session import Session
+from flask.setAPI import *
+
+app = Flask(__name__)
+api = Api(app)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Custom filter
+# app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(VariableRouting, '/var/<string:id>')
+
+if __name__ == '__main__':
+    app.run(debug = True)
