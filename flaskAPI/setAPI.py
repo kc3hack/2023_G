4c1d8c44eaf15d0801dc880987f0af87bb9f6c48
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2023-02-16 01:04:59 +0900

    Add: requirements.txt added
    必要なやつのテキスト追加するの忘れてた

--- flaskAPI/setAPI.py
+++ flaskAPI/setAPI.py
@@ -6,12 +6,5 @@ class HelloWorld(Resource):
     def get(self):
         return { 'message': 'Hello World' }
 
-class VariableRouting(Resource):
-    def get(self, id):
-        return { 'id': id }
-
-class userSession(Resource):
-    def get(self, id):
-        return { 'id': id }
 
 
