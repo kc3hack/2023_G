c268a5df74d34398557e8dc9661a25a4cfcf50f9
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2023-02-16 13:48:00 +0900

    Update: get function
    getのところの処理を元に戻した

--- flaskAPI/sqldata/manegeNotice.py
+++ flaskAPI/sqldata/manegeNotice.py
@@ -38,7 +38,7 @@ class Notion(Resource):
                 "channelId":task.id
             }
             response.append(hash)
-        response = json.dump(response)
+        # response = json.dump(response)
         return response
 
     def post(self):
