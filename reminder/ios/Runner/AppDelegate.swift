86d5081499cc0c64ce39c088e851fa21b3d16246
Lona <103243217+DoLonaAnd@users.noreply.github.com>
2023-02-16 18:18:30 +0900

    Add sending notification method for iOS(Non-test).

--- reminder/ios/Runner/AppDelegate.swift
+++ reminder/ios/Runner/AppDelegate.swift
@@ -7,6 +7,14 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Notification Setting
+    if #available(iOS 10.0, *){
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+    GeneratedPluginRegistrant.register(with: registry)
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- reminder/lib/common/send_local_notification.dart
+++ reminder/lib/common/send_local_notification.dart
@@ -21,7 +21,7 @@ class LocalNotifications {
   }
 
 /*
-  // 以下略
+  // Androidでの通知アクション
   Future<void> _showNotificationWithActions() async {
     const AndroidNotificationDetails androidNotificationDetails =
         AndroidNotificationDetails(
@@ -40,27 +40,53 @@ class LocalNotifications {
         .show(0, '...', '...', notificationDetails);
   }
 */
-  //　通知タップ時メソッド
   Future<void> onSelectNotification(String? payload) async {}
-  // アプリがバックグラウンドや終了時の通知タップ時メソッド
   Future<void> onDidReceiveLocalNotification(
       int id, String? title, String? body, String? payload) async {}
+  // 通知タップ時のメソッド
+  void onDidReceiveNotificationResponse(
+      NotificationResponse notificationResponse) async {
+    final String? payload = notificationResponse.payload;
+    if (notificationResponse.payload != null) {
+      print('notification payload: $payload');
+    }
+    // 画面遷移
+    /*
+    await Navigator.push(
+      context,
+      MaterialPageRoute<void>(builder: (context) => SecondScreen(payload)),
+    );
+    */
+  }
+
   // 初期化メソッド
   Future<void> Initialization() async {
-    const String iconImage = '@mipmap/ic_launcher3';
+    const String iconImage = 'icon path';
     if (flutterLocalNotificationsPlugin != null) {
       return;
     }
     flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-    /*flutterLocalNotificationsPlugin
+    var resultAndroid = await flutterLocalNotificationsPlugin!
         .resolvePlatformSpecificImplementation<
-            AndroidFlutterLocalNotificationsPlugin>()
-        .requestPermission();*/
+            AndroidFlutterLocalNotificationsPlugin>()!
+        .requestPermission();
+    var resultIOS = await flutterLocalNotificationsPlugin!
+        .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
+    print(resultAndroid);
+    print(resultIOS);
     //タイムゾーン設定
     await initTimeZone();
     // 初期化処理実行
+    // Android
     const AndroidInitializationSettings androidInitializationSettings =
         AndroidInitializationSettings(iconImage);
+    // iOS
     final DarwinInitializationSettings darwinInitializationSettings =
         DarwinInitializationSettings(
       requestSoundPermission: false,
@@ -75,11 +101,9 @@ class LocalNotifications {
     flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
     await flutterLocalNotificationsPlugin!.initialize(
       initializationSettings,
-      //onDidReceiveNotificationResponse: onDidReceiveLocalNotificationResponse,
       onDidReceiveNotificationResponse:
-          (NotificationResponse notificationRespinse) async {
-        //onDidReceiveBackgroundNotificationResponse: notificationTapBackground,
-      },
+          (NotificationResponse notificationResponse) async {},
+      onDidReceiveBackgroundNotificationResponse: notificationTapBackground,
     );
 
     return;
@@ -114,15 +138,21 @@ class LocalNotifications {
           channelID,
           channelName,
           channelDescription: "Channel Description.",
+          //importance: Importance.max,
+          //priority: Priority.high,
+          //ticker: 'ticker', // 旧Androidのステータスバーに表示
         ),
-        //iOS: IOSNotificationDetails(),
+        iOS: DarwinNotificationDetails(),
       );
       // 通知送信予約設定
-      await flutterLocalNotificationsPlugin!.zonedSchedule(
+      /*await flutterLocalNotificationsPlugin!.zonedSchedule(
           1, title, body, scheduledDate, notificationDetails,
           androidAllowWhileIdle: true,
           uiLocalNotificationDateInterpretation:
-              UILocalNotificationDateInterpretation.absoluteTime);
+              UILocalNotificationDateInterpretation.absoluteTime);*/
+      // 通知送信
+      await flutterLocalNotificationsPlugin!
+          .show(0, title, body, notificationDetails, payload: 'item x');
       return true;
     } catch (e) {
       print(e.toString());
