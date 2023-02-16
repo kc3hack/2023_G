c0ee647595f75b01536f91dba73ee16f1146a044
MrMocchy <patronummocchy@tiara.ocn.n.jp>
2023-02-16 20:18:55 +0900

    flaskのデータベースへの保存と取り出し成功

--- reminder/lib/com_with_flask.dart
+++ reminder/lib/com_with_flask.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -7,44 +6,6 @@ import 'package:http/http.dart' as http;
 import 'flutter_data.dart';
 
 //cf https://stackoverflow.com/questions/65630743/how-to-solve-flutter-web-api-cors-error-only-with-dart-code
-
-Future<void> getHelloWorld() async {
-  var jsondata = await getDatafromSubURL('');
-  debugPrint(jsondata);
-}
-
-Future<void> getRandomId() async {
-  final responce = await http
-      .get(Uri.parse('http://127.0.0.1:5000/var/${Random().nextInt(100)}'));
-  debugPrint(responce.statusCode.toString());
-  debugPrint(responce.body);
-}
-
-Future<dynamic> getDatafromFullURL(String fullurl) async {
-  var response = await http.get(Uri.parse(fullurl));
-  debugPrint('StatusCode:${response.statusCode.toString()}');
-  //if (response.statusCode != 200) return;
-  return jsonDecode(response.body);
-}
-
-Future<dynamic> postDatafromFullURL(
-    String fullurl, Map<String, String> map) async {
-  var response = await http.post(Uri.parse(fullurl), body: jsonEncode(map));
-  debugPrint('StatusCode:${response.statusCode.toString()}');
-  //if (response.statusCode != 200) return;
-  return jsonDecode(response.body);
-}
-
-Future<dynamic> getDatafromSubURL(String suburl) async {
-  String fullurl = 'http://127.0.0.1:5000$suburl';
-  return getDatafromFullURL(fullurl);
-}
-
-Future<dynamic> postDatafromSubURL(
-    String suburl, Map<String, String> map) async {
-  String fullurl = 'http://127.0.0.1:5000$suburl';
-  return postDatafromFullURL(fullurl, map);
-}
 
 class ComTestPage extends StatefulWidget {
   const ComTestPage({super.key});
@@ -60,14 +21,15 @@ class _ComTestPageState extends State<ComTestPage> {
 
   var n = NotificationData(
       setDateTime: DateTime.now(),
-      limitDateTime: DateTime.now().add(const Duration(days: 10)),
-      title: "test",
-      description: "desc",
-      //remindList: [DateTime(2023, 2, 15), DateTime(2023, 2, 22)],
-      channelID: 123);
+      //  limitDateTime: DateTime.now().add(const Duration(days: 10)),
+      content: "test",
+      // description: "desc",
+      remindList: [DateTime(2023, 2, 15), DateTime(2023, 2, 22)],
+      channelId: 123);
 
   @override
   Widget build(BuildContext context) {
+    Schedule schedule = Schedule();
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -108,29 +70,59 @@ class _ComTestPageState extends State<ComTestPage> {
               child: const Text('get'),
             ),
             FloatingActionButton(
-              onPressed: () {
-                Future(() async {
-                  final res = await postDatafromSubURL('/notion', n.toMap());
-                  debugPrint(res.toString());
-                });
+              onPressed: () async {
+                final res = await n.saveIntoDatabase();
+                debugPrint(res.toString());
               },
-              child: const Text('post'),
+              child: const Text('n post'),
+            ),
+            FloatingActionButton(
+              onPressed: () async {
+                await schedule.getFromDatabase();
+              },
+              child: const Text('s get'),
             ),
             FloatingActionButton(
               onPressed: () {
                 Future(() async {
                   var response = await http.post(
-                      Uri.parse('http://172.0.0.1:5000/notion'),
-                      body: jsonEncode({
+                      Uri.parse('http://127.0.0.1:5000/notion'),
+                      headers: {
+                        'content-type': 'application/json'
+                      },
+                      body: {
                         "content": "con",
                         "setDateTime": "2023-02-16 13:00:00"
-                      }));
+                      });
                   debugPrint('StatusCode:${response.statusCode.toString()}');
-                  if (response.statusCode != 200) return;
                   debugPrint(response.body.toString());
                 });
               },
-              child: const Text('post2'),
+              child: const Text('post map'),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                Future(() async {
+                  debugPrint(jsonEncode({
+                    "content": "con",
+                    "setDateTime": "2023-02-16 13:00:00"
+                  }));
+                  var response =
+                      await http.post(Uri.parse('http://127.0.0.1:5000/notion'),
+                          headers: {'content-type': 'application/json'},
+                          body: jsonEncode({
+                            "content": "con",
+                            "setDateTime": "2023-02-16 13:00:00",
+                            "remindList": [
+                              "2023-02-17 20:00:00",
+                              "2023-03-01 10:00:00"
+                            ]
+                          }));
+                  debugPrint('StatusCode:${response.statusCode.toString()}');
+                  debugPrint(response.body.toString());
+                });
+              },
+              child: const Text('post json'),
             ),
           ],
         ),
--- reminder/lib/flutter_data.dart
+++ reminder/lib/flutter_data.dart
@@ -1,26 +1,43 @@
 import 'dart:convert';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+final notionUri = Uri.parse('http://127.0.0.1:5000/notion');
+final notionHeader = {'content-type': 'application/json'};
 
 class NotificationData {
   late DateTime setDateTime; //設定した日時
-  late DateTime limitDateTime; //期限
-  late String title; //通知のタイトル
-  late String description; //詳細
-  late int channelID; //通知チャンネルID
+  //late DateTime limitDateTime; //期限
+  late String content; //通知のタイトル
+  //late String description; //詳細
+  late int channelId; //通知チャンネルID
+  late List<DateTime> remindList;
 
   NotificationData(
       {required this.setDateTime,
-      required this.limitDateTime,
-      required this.title,
-      required this.description,
-      required this.channelID});
+      //required this.limitDateTime,
+      required this.content,
+      required this.remindList,
+      //required this.description,
+      required this.channelId});
 
-  Map<String, String> toMap() {
+  static dateTimeToString(DateTime dt) {
+    String zp(int n) {
+      return n.toString().padLeft(2, '0');
+    }
+
+    return '${dt.year}-${zp(dt.month)}-${zp(dt.day)} ${zp(dt.hour)}:${zp(dt.minute)}:${zp(dt.second)}';
+  }
+
+  Map<String, dynamic> toMap() {
     return {
-      "setDateTime": setDateTime.toString(),
-      "limitDateTime": limitDateTime.toString(),
-      "content": title,
-      "description": description,
-      "channelID": channelID.toString(),
+      "setDateTime": dateTimeToString(setDateTime),
+      //"limitDateTime": limitDateTime.toString(),
+      "content": content,
+      "remindList": remindList.map((e) => dateTimeToString(e)).toList(),
+      //"description": description,
+      "channelId": channelId.toString(),
     };
   }
 
@@ -30,13 +47,89 @@ class NotificationData {
 
   static NotificationData fromJsonString(String jstr) {
     final json = jsonDecode(jstr);
+    List<dynamic> sl = json["remindList"];
+    List<DateTime> dl = sl.map((e) => DateTime.parse(e.toString())).toList();
     return NotificationData(
         setDateTime: DateTime.parse(json["setDateTime"].toString()),
-        limitDateTime: DateTime.parse(json["limitDateTime"].toString()),
-        title: json["title"] as String,
-        description: json["description"] as String,
-        channelID: json["channelID"] as int);
+        //limitDateTime: DateTime.parse(json["limitDateTime"].toString()),
+        content: json["content"] as String,
+        //description: json["description"] as String,
+        remindList: dl,
+        channelId: json["channelId"] as int);
+  }
+
+  Future<String> saveIntoDatabase() async {
+    debugPrint("■" + toJsonString());
+    var response =
+        await http.post(notionUri, headers: notionHeader, body: toJsonString());
+    debugPrint('StatusCode:${response.statusCode.toString()}');
+    if (response.statusCode == 500) return '';
+    return response.body;
+  }
+
+  void print() {
+    debugPrint('Notification print');
+    debugPrint(toJsonString());
   }
 }
 
-class Schedule {}
+class Schedule {
+  List<NotificationData> list = [];
+
+  Future<void> getFromDatabase() async {
+    var response = await http.get(notionUri, headers: notionHeader);
+    debugPrint('StatusCode:${response.statusCode.toString()}');
+    if (response.statusCode != 200) return;
+    String short = response.body.substring(7, response.body.length - 4);
+    List<String> jlist =
+        short.split(RegExp(r'},\s+{')).map((e) => '{$e}').toList();
+    list = jlist.map((e) => NotificationData.fromJsonString(e)).toList();
+    print();
+  }
+
+  void print() {
+    debugPrint('Schelude print');
+    debugPrint(list.map((e) => e.toJsonString()).toList().toString());
+  }
+}
+
+//以下テスト用
+
+Future<void> getHelloWorld() async {
+  var jsondata = await getDatafromSubURL('');
+  debugPrint(jsondata);
+}
+
+Future<void> getRandomId() async {
+  final responce = await http
+      .get(Uri.parse('http://127.0.0.1:5000/var/${Random().nextInt(100)}'));
+  debugPrint(responce.statusCode.toString());
+  debugPrint(responce.body);
+}
+
+Future<dynamic> getDatafromFullURL(String fullurl) async {
+  var response = await http
+      .get(Uri.parse(fullurl), headers: {'content-type': 'application/json'});
+  debugPrint('StatusCode:${response.statusCode.toString()}');
+  //if (response.statusCode != 200) return;
+  return jsonDecode(response.body);
+}
+
+Future<dynamic> postDataToFullURL(
+    String fullurl, Map<String, String> map) async {
+  var response = await http.post(Uri.parse(fullurl),
+      headers: {'content-type': 'application/json'}, body: jsonEncode(map));
+  debugPrint('StatusCode:${response.statusCode.toString()}');
+  //if (response.statusCode != 200) return;
+  return jsonDecode(response.body);
+}
+
+Future<dynamic> getDatafromSubURL(String suburl) async {
+  String fullurl = 'http://127.0.0.1:5000$suburl';
+  return getDatafromFullURL(fullurl);
+}
+
+Future<dynamic> postDataToSubURL(String suburl, Map<String, String> map) async {
+  String fullurl = 'http://127.0.0.1:5000$suburl';
+  return postDataToFullURL(fullurl, map);
+}
