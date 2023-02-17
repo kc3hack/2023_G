b7293e9099acde7069ebf90db3af5167ef269800
Lona <103243217+DoLonaAnd@users.noreply.github.com>
2023-02-18 07:08:07 +0900

    organize functions.

--- reminder/lib/common/send_local_notification.dart
+++ reminder/lib/common/send_local_notification.dart
@@ -1,7 +1,4 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
-/*import 'package:flutter_native_timezone/flutter_native_timezone.dart';
-*/
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
@@ -9,11 +6,11 @@ class LocalNotifications {
   static FlutterLocalNotificationsPlugin? flutterLocalNotificationsPlugin =
       null;
 
+  // 通知タップ時起爆メソッド
   @pragma('vm:entry-point')
-  void notificationTapBackground(NotificationResponse notificationResponse) {
-    // handle action
-  }
-  // タイムゾーン設定メソッド
+  void notificationTap(NotificationResponse notificationResponse) {}
+
+  // タイムゾーン初期化メソッド
   Future<void> initTimeZone() async {
     tz.initializeTimeZones();
     var jp = tz.getLocation('Asia/Tokyo');
@@ -21,7 +18,9 @@ class LocalNotifications {
   }
 
 /*
-  // Androidでの通知アクション
+  * Androidでの通知アクション
+  * よく分かっておらず、無くても動作はする。
+
   Future<void> _showNotificationWithActions() async {
     const AndroidNotificationDetails androidNotificationDetails =
         AndroidNotificationDetails(
@@ -40,31 +39,25 @@ class LocalNotifications {
         .show(0, '...', '...', notificationDetails);
   }
 */
+  // なんのためのメソッドかよくわからない三銃士
   Future<void> onSelectNotification(String? payload) async {}
   Future<void> onDidReceiveLocalNotification(
       int id, String? title, String? body, String? payload) async {}
-  // 通知タップ時のメソッド
   void onDidReceiveNotificationResponse(
       NotificationResponse notificationResponse) async {
     final String? payload = notificationResponse.payload;
     if (notificationResponse.payload != null) {
       print('notification payload: $payload');
     }
-    // 画面遷移
-    /*
-    await Navigator.push(
-      context,
-      MaterialPageRoute<void>(builder: (context) => SecondScreen(payload)),
-    );
-    */
   }
 
   // 初期化メソッド
-  Future<void> Initialization() async {
-    const String iconImage = 'icon path';
+  Future<void> Initialization(String iconPath) async {
     if (flutterLocalNotificationsPlugin != null) {
+      // 初期化済みの場合キャンセルされる。
       return;
     }
+    final String iconImage = iconPath;
     flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
     var resultAndroid = await flutterLocalNotificationsPlugin!
         .resolvePlatformSpecificImplementation<
@@ -82,9 +75,9 @@ class LocalNotifications {
     print(resultIOS);
     //タイムゾーン設定
     await initTimeZone();
-    // 初期化処理実行
+    // 初期化処理設定
     // Android
-    const AndroidInitializationSettings androidInitializationSettings =
+    final AndroidInitializationSettings androidInitializationSettings =
         AndroidInitializationSettings(iconImage);
     // iOS
     final DarwinInitializationSettings darwinInitializationSettings =
@@ -92,38 +85,53 @@ class LocalNotifications {
       requestSoundPermission: false,
       requestBadgePermission: false,
       requestAlertPermission: false,
-      //onDidReceiveLocalNotifications: onDidReceiveLocalNotifications,
+      onDidReceiveLocalNotification: onDidReceiveLocalNotification,
     );
+    // 初期化設定インスタンス
     InitializationSettings initializationSettings = InitializationSettings(
       android: androidInitializationSettings,
       iOS: darwinInitializationSettings,
+      //macOS: darwinInitializationSettings,
     );
+    // Pluginインスタンス生成
     flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+    // 初期化処理実行
     await flutterLocalNotificationsPlugin!.initialize(
       initializationSettings,
-      onDidReceiveNotificationResponse:
-          (NotificationResponse notificationResponse) async {},
-      onDidReceiveBackgroundNotificationResponse: notificationTapBackground,
+      onDidReceiveNotificationResponse: notificationTap, // 通知タップ時
     );
 
     return;
   }
 
-  // 通知設定
+  // 通知送信メソッド
   Future<bool> SendLocalNotification(
-    String title, // 通知タイトル
-    String body, // 通知内容
-    DateTime dayTime, // 通知送信時刻
-  ) async {
+      String title, // 通知タイトル
+      String body, // 通知内容
+      DateTime dayTime, // 通知送信時刻
+      String? iconPath // 通知アイコンパス
+      ) async {
+    // 各種変数
     String channelID = "LocalNotification";
     String channelName = "SpecifiedNotification";
-    String icon = "@drawable/favicon";
-    if (flutterLocalNotificationsPlugin == null) {
-      return false;
+    String icon = "@drawable/favicon"; // App Icon
+    bool flag = false; // 通知送信可否
+
+    if (iconPath == null) {
+      iconPath = '@mipmap-xxxhdpi/ic_launcher';
     }
+
+    if (flutterLocalNotificationsPlugin == null) {
+      return flag;
+    }
+
+    // タイムゾーン初期化
     initTimeZone();
-    Initialization();
+    // 通知設定初期化
+    Initialization(iconPath);
+
     try {
+      // 通知予約時間設定
       tz.TZDateTime scheduledDate = tz.TZDateTime(
         tz.local,
         dayTime.year,
@@ -133,6 +141,7 @@ class LocalNotifications {
         dayTime.minute,
         dayTime.second,
       );
+      // 通知設定
       NotificationDetails notificationDetails = NotificationDetails(
         android: AndroidNotificationDetails(
           channelID,
@@ -143,20 +152,21 @@ class LocalNotifications {
           //ticker: 'ticker', // 旧Androidのステータスバーに表示
         ),
         iOS: DarwinNotificationDetails(),
+        //macOS: DarwinNotificationDetails(),
       );
-      // 通知送信予約設定
+      // 通知予約送信
       await flutterLocalNotificationsPlugin!.zonedSchedule(
           1, title, body, scheduledDate, notificationDetails,
           androidAllowWhileIdle: true,
           uiLocalNotificationDateInterpretation:
               UILocalNotificationDateInterpretation.absoluteTime);
-      // 通知送信
+      // 通知即時送信
       /*await flutterLocalNotificationsPlugin!
           .show(0, title, body, notificationDetails, payload: 'item x');*/
-      return true;
+      flag = true;
     } catch (e) {
       print(e.toString());
-      return false;
     }
+    return flag;
   }
 }
