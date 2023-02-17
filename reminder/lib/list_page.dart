facfb862f6deb519ea1ed0c7963d94ddb6b95739
MrMocchy <patronummocchy@tiara.ocn.n.jp>
2023-02-17 17:03:10 +0900

    リストページの調整、デリート機能の作成

--- reminder/lib/list_page.dart
+++ reminder/lib/list_page.dart
@@ -12,13 +12,35 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
+  final buttonStyle =
+      const ButtonStyle(fixedSize: MaterialStatePropertyAll(Size(50, 40)));
+
+  //テスト用通知データ
   var n = NotificationData(
-      setDateTime: DateTime.now(),
-      //  limitDateTime: DateTime.now().add(const Duration(days: 10)),
+      createDateTime: DateTime.now().add(const Duration(days: 1)),
+      limitDateTime: DateTime.now().add(const Duration(days: 20)),
       content: "test",
       // description: "desc",
-      remindList: [DateTime(2023, 2, 15), DateTime(2023, 2, 22)],
+      remindList: [DateTime(2023, 2, 20), DateTime(2023, 2, 25)],
       channelId: 123);
+
+  void addSchedule() async {
+    //Todo:予定追加画面に遷移する
+    //現在テスト用
+    await n.saveIntoDatabase();
+    reload();
+  }
+
+  void reload() async {
+    await schedule.getFromDatabase();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    reload();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,24 +56,28 @@ class _ListPageState extends State<ListPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                ElevatedButton(
-                  onPressed: () async {
-                    await n.saveIntoDatabase();
-                    setState(() {});
-                  },
-                  child: const Icon(Icons.add),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: ElevatedButton(
+                    onPressed: addSchedule,
+                    style: buttonStyle,
+                    child: const Icon(Icons.add),
+                  ),
                 ),
-                ElevatedButton(
-                  onPressed: () async {
-                    await schedule.getFromDatabase();
-                    setState(() {});
-                  },
-                  child: const Icon(Icons.refresh),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: ElevatedButton(
+                    onPressed: reload,
+                    style: buttonStyle,
+                    child: const Icon(Icons.refresh),
+                  ),
                 ),
               ],
             ),
+            //一覧表示
             SizedBox(
               height: size.height * 0.7,
+              width: size.width * 0.8,
               child: ListView.builder(
                 itemCount: schedule.list.length,
                 itemBuilder: (context, index) {
@@ -60,8 +86,11 @@ class _ListPageState extends State<ListPage> {
                       title: SizedBox(
                         height: 60,
                         child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text(
                                   schedule.list[index].content,
@@ -70,17 +99,22 @@ class _ListPageState extends State<ListPage> {
                                       fontWeight: FontWeight.bold,
                                       color: Colors.black),
                                 ),
+                                const SizedBox(height: 5),
                                 Text(
-                                  NotificationData.dateTimeToString(
-                                      schedule.list[index].setDateTime),
+                                  'limit:${NotificationData.dateTimeToString(schedule.list[index].limitDateTime)}',
                                   style: const TextStyle(
                                       fontSize: 15, color: Colors.black54),
                                 )
                               ],
                             ),
+                            //削除ボタン
                             IconButton(
-                                onPressed: () {
-                                  //キャンセル
+                                onPressed: () async {
+                                  //Delete
+                                  await NotificationData.deleteNotion(
+                                      schedule.list[index].channelId);
+                                  //Todo:local_notificationの方も削除する
+                                  reload();
                                 },
                                 icon: const Icon(Icons.delete))
                           ],
