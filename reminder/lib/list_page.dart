58df5beeb0504babb3e3de167694a9fdec91457f
MrMocchy <patronummocchy@tiara.ocn.n.jp>
2023-02-18 16:22:35 +0900

    画面からはみ出る問題の修正

--- reminder/lib/list_page.dart
+++ reminder/lib/list_page.dart
@@ -52,13 +52,20 @@ class _ListPageState extends State<ListPage> {
       return PulldownBTNPage();
     }));
     //テスト用
-    //await rndnd().saveIntoDatabase();
+    await rndnd().saveIntoDatabase();
     reload();
   }
 
   Future<void> reload() async {
     await schedule.getFromDatabase();
     setState(() {});
+  }
+
+  String lastDuration(Duration d) {
+    if (d.inDays > 0) return "あと${d.inDays}日";
+    if (d.inHours > 0) return "あと${d.inHours}時間";
+    if (d.inMinutes > 0) return "あと${d.inMinutes}分";
+    return "期限切れ";
   }
 
   @override
@@ -80,146 +87,154 @@ class _ListPageState extends State<ListPage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(
-                  width: windowSize.width * 0.9,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        child: ElevatedButton.icon(
-                          onPressed: addSchedule,
-                          style: const ButtonStyle(
-                              backgroundColor:
-                                  MaterialStatePropertyAll(Colors.green),
-                              fixedSize:
-                                  MaterialStatePropertyAll(Size(170, 40))),
-                          label: const Text(
-                            '予定を作成',
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          icon: const Icon(
-                            Icons.add,
-                            size: 30,
-                          ),
+      body: Stack(children: [
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                width: windowSize.width * 0.9,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      child: ElevatedButton.icon(
+                        onPressed: addSchedule,
+                        style: const ButtonStyle(
+                            backgroundColor:
+                                MaterialStatePropertyAll(Colors.green),
+                            fixedSize: MaterialStatePropertyAll(Size(170, 40))),
+                        label: const Text(
+                          '予定を作成',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        icon: const Icon(
+                          Icons.add,
+                          size: 30,
                         ),
                       ),
-                      Row(children: [
-                        const Text('削除ボタン：'),
-                        //cf: https://stackoverflow.com/questions/45924474/how-do-you-detect-the-host-platform-from-dart-code
-                        Theme.of(context).platform == TargetPlatform.iOS
-                            ? CupertinoSwitch(
-                                value: enableDeleteButton,
-                                activeColor: Colors.red,
-                                onChanged: (b) {
-                                  setState(() {
-                                    enableDeleteButton = b;
-                                  });
-                                })
-                            : Switch(
-                                value: enableDeleteButton,
-                                activeColor: Colors.red,
-                                onChanged: (b) {
-                                  setState(() {
-                                    enableDeleteButton = b;
-                                  });
-                                }),
-                      ]),
-                    ],
-                  ),
+                    ),
+                    Row(children: [
+                      const Text('削除ボタン：'),
+                      //cf: https://stackoverflow.com/questions/45924474/how-do-you-detect-the-host-platform-from-dart-code
+                      Theme.of(context).platform == TargetPlatform.iOS
+                          ? CupertinoSwitch(
+                              value: enableDeleteButton,
+                              activeColor: Colors.red,
+                              onChanged: (b) {
+                                setState(() {
+                                  enableDeleteButton = b;
+                                });
+                              })
+                          : Switch(
+                              value: enableDeleteButton,
+                              activeColor: Colors.red,
+                              onChanged: (b) {
+                                setState(() {
+                                  enableDeleteButton = b;
+                                });
+                              }),
+                    ]),
+                  ],
                 ),
-                //一覧表示
-                SizedBox(
-                  height: windowSize.height - 200,
-                  width: windowSize.width * 0.9,
-                  child: schedule.list.length == 0
-                      ? Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: const [
-                              SizedBox(
-                                height: 50,
-                              ),
-                              Text(
-                                '現在登録されている予定はありません。',
-                                style: TextStyle(fontSize: 20),
-                              )
-                            ])
-                      : ListView.builder(
-                          itemCount: schedule.list.length,
-                          itemBuilder: (context, index) {
-                            return Card(
-                              elevation: 5,
-                              child: ListTile(
-                                title: SizedBox(
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.start,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          const SizedBox(height: 10),
-                                          Text(
-                                            schedule.list[index].content,
-                                            style: const TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.black),
-                                          ),
-                                          const SizedBox(height: 5),
-                                          Text(
-                                              schedule.list[index].description),
-                                          const SizedBox(height: 5),
-                                          Text(
-                                            '${NotificationData.dateTimeToString(schedule.list[index].limitDateTime)}',
-                                            style: const TextStyle(
-                                                fontSize: 15,
-                                                color: Colors.black87),
-                                          )
-                                        ],
-                                      ),
-                                      //削除ボタン
-                                      enableDeleteButton
-                                          ? IconButton(
-                                              iconSize: 30,
-                                              onPressed: () async {
-                                                //Delete
-                                                await NotificationData
-                                                    .deleteNotion(schedule
-                                                        .list[index].channelId);
-                                                reload();
-                                              },
-                                              icon: const Icon(
-                                                Icons.delete,
-                                                color: Colors.red,
-                                              ))
-                                          : Container(),
-                                    ],
-                                  ),
+              ),
+              //一覧表示
+              SizedBox(
+                height: windowSize.height - 150,
+                width: windowSize.width * 0.9,
+                child: schedule.list.length == 0
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: const [
+                            SizedBox(
+                              height: 50,
+                            ),
+                            Text(
+                              '現在登録されている予定はありません。',
+                              style: TextStyle(fontSize: 20),
+                            )
+                          ])
+                    : ListView.builder(
+                        itemCount: schedule.list.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            elevation: 5,
+                            child: ListTile(
+                              title: SizedBox(
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        const SizedBox(height: 10),
+                                        Text(
+                                          schedule.list[index].content,
+                                          style: const TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.black),
+                                        ),
+                                        const SizedBox(height: 5),
+                                        Text(schedule.list[index].description),
+                                        const SizedBox(height: 5),
+                                        Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                '${NotificationData.dateTimeToString(schedule.list[index].limitDateTime)}',
+                                                style: const TextStyle(
+                                                    fontSize: 15,
+                                                    color: Colors.black87),
+                                              ),
+                                              const SizedBox(width: 15),
+                                              Text(lastDuration(schedule
+                                                  .list[index].limitDateTime
+                                                  .difference(DateTime.now())))
+                                            ])
+                                      ],
+                                    ),
+                                    //削除ボタン
+                                    enableDeleteButton
+                                        ? IconButton(
+                                            iconSize: 30,
+                                            onPressed: () async {
+                                              //Delete
+                                              await NotificationData
+                                                  .deleteNotion(schedule
+                                                      .list[index].channelId);
+                                              reload();
+                                            },
+                                            icon: const Icon(
+                                              Icons.delete,
+                                              color: Colors.red,
+                                            ))
+                                        : Container(),
+                                  ],
                                 ),
                               ),
-                            );
-                          },
-                        ),
-                ),
-              ],
-            ),
-            footer,
-          ],
+                            ),
+                          );
+                        },
+                      ),
+              ),
+            ],
+          ),
         ),
-      ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [footer],
+        ),
+      ]),
     );
   }
 }
