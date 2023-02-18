f1f722c5ef5cc0e3b4ba2d801492752fb5e4c5b6
def-FkSgrKN <sogofurukawafs.ks2020.jp@gmail.com>
2023-02-18 20:31:55 +0900

    flaskとOCRを繋ぎます。

---
+++ flaskAPI/sqldata/img_base64.py
@@ -0,0 +1,35 @@
+import cv2
+import base64
+import numpy as np
+import io
+
+# Qiita, [Python]Base64でエンコードされた画像データをデコードする。
+# https://qiita.com/TsubasaSato/items/908d4f5c241091ecbf9b, 2023年2月16日
+
+# Qiita, base64ってなんぞ？？理解のために実装してみた
+# https://qiita.com/PlanetMeron/items/2905e2d0aa7fe46a36d4
+
+#r"" raw文字列では//や/nがそのまま認識されるため、改行文字などのエスケープ文字にならない。
+
+
+
+
+
+def base64_to_img(img_binary, img_path):
+
+    #バイナリデータ <- base64でエンコードされたデータ  
+    img_ndry=np.frombuffer(img_binary,dtype=np.uint8) #バッファ―をnumpy配列に高速変換している
+
+    #raw image <- img_ndry
+    img = cv2.imdecode(img_ndry, cv2.IMREAD_COLOR)
+
+    return img
+
+
+    #画像を保存する場合
+    #cv2.imwrite(img_path, img)
+
+    #表示確認
+    #cv2.imshow('window title', img)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
--- flaskAPI/sqldata/manegeNotice.py
+++ flaskAPI/sqldata/manegeNotice.py
@@ -1,10 +1,21 @@
-from sqldata.init import notion
-from sqldata.checkModel import ModelOperate
+import base64
+from distutils.log import debug
+from torch import import_ir_module
+#from sqldata.init import notion
+#from sqldata.checkModel import ModelOperate
+from init import notion
+from checkModel import ModelOperate
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from datetime import datetime, timedelta, timezone
 from flask_restful import Resource
 from flask import json, request
+
+#追加
+from img_base64 import base64_to_img
+from ocr_test_data import ocr_process
+
 
 JST = timezone(timedelta(hours=+9), 'JST')
 datetime.now(JST)
@@ -80,18 +91,59 @@ class ReceiveBase64(Resource):
 # OCR用の画像データ取得をする
     def post(self):
         input = request.json
+
         base64data = input['base64Image']#ここにbase64のデータが入ってます．
         
+
+        img_path = "memo_kurozi.png"
+        with open(img_path, 'rb') as f:
+            imgdata = f.read()
+
+        #Base64で画像をエンコード
+        base64data=base64.b64encode(imgdata)
+        
+
+        img = base64_to_img(base64data)
+
+
+        OCR_path=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+        yotei_list = ocr_process(OCR_path, img)
+        
+        
+        #print(yotei_list)
+
         """
         ここにOCRの操作が来る
         読み取ったデータは以下の形になる
         """
-        response = {
-            # 'contet':'通知タイトル',
-            # 'limitDateTime':DATETIME.strftime('%Y-%m-%d %H:%M:%S')
-            'test':'aaaaaaaaaaa'
-        }
 
+        if len(yotei_list) == 1:
+            response = {
+                'contet':yotei_list[0],
+                'limitDateTime':yotei_list[1].strftime('%Y-%m-%d %H:%M:%S')
+
+                #'contet':'通知タイトル',
+                #'limitDateTime':DATETIME.strftime('%Y-%m-%d %H:%M:%S')
+                #'test':'aaaaaaaaaaa'
+            }
+
+        elif len(yotei_list) > 1:
+
+            response = {
+                'contet':yotei_list[0][0],
+                'limitDateTime':yotei_list[0][1]
+
+                #'contet':'通知タイトル',
+                #'limitDateTime':DATETIME.strftime('%Y-%m-%d %H:%M:%S')
+                #'test':'aaaaaaaaaaa'
+            }
+
+    
         return response
 
 
+    def main():
+        rcvB64 = ReceiveBase64(Resource)
+        rcvB64.post()
+        
+        
